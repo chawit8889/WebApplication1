86ad9acc3ec4618eef249aa2ba284270952d857a
chawit8889 <61845133+chawit8889@users.noreply.github.com>
2020-05-22 23:06:26 +0700

    22/05/20

--- WebApplication1/Controllers/ItemController.cs
+++ WebApplication1/Controllers/ItemController.cs
@@ -3,14 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class ItemController : Controller
     {
+        //ค้นหา
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string itemid, string itemname )
+        {
+            TestContext context = new TestContext();
+            if(itemname==null)
+            {
+                itemname = "";
+            }
+            var items = context.Item
+                .Where(i=>i.ItemId.ToString().Contains(itemid)
+                &&i.ItemName .Contains(itemname ))
+              .ToList();
+            return View(items);
+        }
+        [HttpGet]
+        public IActionResult InsertItem()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult InsertItem
+             (Item item)
+        {
+            TestContext context = new TestContext();
+            context.Item.Add(item);
+            context.SaveChanges();
+
+            return Json(item);
+
         }
     }
 }

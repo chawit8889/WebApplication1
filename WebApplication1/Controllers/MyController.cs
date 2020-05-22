25b8e2f7200b33d5c3453deffa6dd8c4d4e0ab03
chawit8889 <61845133+chawit8889@users.noreply.github.com>
2020-05-22 20:58:22 +0700

    Add project files.

---
+++ WebApplication1/Controllers/MyController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MyController : Controller
+    {
+        public string Index()
+        {
+            return"Hello My Controller";
+        }
+        public string HelloWorld(int id,string name)
+        {
+            return "Hello My Controller"+id+",name"+name;
+        }
+    }
+}
